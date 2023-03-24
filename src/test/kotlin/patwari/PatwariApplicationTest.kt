2cca8af32cd5cd192fb2737c48ae73a3c535690d
mehtabhassan <mehtabhassan1995@gmail.com>
2023-03-24 22:51:42 +0500

    Updated context test

--- src/test/kotlin/patwari/PatwariApplicationTest.kt
+++ src/test/kotlin/patwari/PatwariApplicationTest.kt
@@ -3,12 +3,19 @@ package patwari
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.util.SocketUtils
 import pk.com.patwari.PatwariApplication
 
 @ActiveProfiles("test")
 @SpringBootTest(classes = [PatwariApplication::class])
 class PatwariApplicationTest {
 
+    companion object {
+        init {
+            System.setProperty("spring.profiles.active", "test")
+            System.setProperty("server.port", SocketUtils.findAvailableTcpPort().toString())
+        }
+    }
     @Test
     fun contextLoads() {
         PatwariApplication.main(arrayOf())
