a21a9f2465b942c50dc062de1bbdfef48183e5ce
mehtabhassan <mehtabhassan1995@gmail.com>
2023-03-26 00:10:08 +0500

    Injected account repo in service class

--- src/main/kotlin/pk/com/patwari/service/AccountService.kt
+++ src/main/kotlin/pk/com/patwari/service/AccountService.kt
@@ -2,10 +2,12 @@ package pk.com.patwari.service
 
 import org.springframework.stereotype.Service
 import pk.com.patwari.dto.response.AccountDetailsResponse
+import pk.com.patwari.repository.AccountRepository
 
 @Service
-class AccountService {
+class AccountService(private val accountRepository: AccountRepository) {
+
     fun getAllAccounts(): List<AccountDetailsResponse>{
-        return listOf(AccountDetailsResponse("some-id"))
+        return accountRepository.findAll().map { AccountDetailsResponse(it.id) }
     }
 }
--- src/test/kotlin/pk/com/patwari/service/AccountServiceTest.kt
+++ src/test/kotlin/pk/com/patwari/service/AccountServiceTest.kt
@@ -4,24 +4,37 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.whenever
+import pk.com.patwari.model.Account
+import pk.com.patwari.repository.AccountRepository
 
+@ExtendWith(MockitoExtension::class)
 class AccountServiceTest{
 
+    @Mock
+    private lateinit var accountRepository: AccountRepository
     private lateinit var service: AccountService
 
     @BeforeEach
     fun setup() {
-        this.service = AccountService()
+        this.service = AccountService(accountRepository)
     }
 
     @Nested
     inner class GetAllAccounts{
+        val account = Account("some-account-id", "some-account-title")
         @Test
         fun shouldReturnAccountList(){
+
+            whenever(accountRepository.findAll()).thenReturn(listOf(account))
 
             val accountList = service.getAllAccounts()
 
             assertEquals(1, accountList.size)
+            assertEquals("some-account-id", accountList[0].id)
         }
     }
 }
