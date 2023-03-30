39dd4eae6bd1cb96eff0c3b79263e0622716297e
mehtabhassan <mehtabhassan1995@gmail.com>
2023-03-30 22:13:17 +0500

    added repo methods for account

--- src/main/kotlin/pk/com/patwari/repository/AccountRepository.kt
+++ src/main/kotlin/pk/com/patwari/repository/AccountRepository.kt
@@ -1,6 +1,9 @@
 package pk.com.patwari.repository
 
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import pk.com.patwari.model.Account
 
@@ -10,4 +13,21 @@ import pk.com.patwari.model.Account
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 @Repository
-interface AccountRepository : CrudRepository<Account, String>
+interface AccountRepository : CrudRepository<Account, String>{
+
+    fun findByAccountNumber(accountNumber: String): Account
+
+    @Modifying
+    @Query("UPDATE Account a " +
+        "SET a.balance = s.balance + :amount, " +
+        "a.updatedAt = CURRENT_TIMESTAMP() " +
+        "WHERE a.id = :accountId")
+    fun addAmount(@Param("accountId") walletNumber: String, @Param("amount") amount: Double): Account
+
+    @Modifying
+    @Query("UPDATE Account a " +
+        "SET a.balance = s.balance - :amount, " +
+        "a.updatedAt = CURRENT_TIMESTAMP() " +
+        "WHERE a.id = :accountId")
+    fun subtractAmount(@Param("accountId") walletNumber: String, @Param("amount") amount: Double): Account
+}
