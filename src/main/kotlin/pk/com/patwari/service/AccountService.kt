903413a2147c6dd49fb8ca362c6dfb9b6fe02031
mehtabhassan <mehtabhassan1995@gmail.com>
2023-03-30 22:13:40 +0500

    Added basic account debit credit logic for accounts

--- src/main/kotlin/pk/com/patwari/service/AccountService.kt
+++ src/main/kotlin/pk/com/patwari/service/AccountService.kt
@@ -1,10 +1,14 @@
 package pk.com.patwari.service
 
 import org.springframework.stereotype.Service
+import pk.com.patwari.constant.AccountType
 import pk.com.patwari.dto.request.AccountCreationRequest
+import pk.com.patwari.dto.request.FundsTransferRequest
 import pk.com.patwari.dto.response.AccountDetailsResponse
+import pk.com.patwari.model.Account
 import pk.com.patwari.model.Account.Companion.toAccountEntity
 import pk.com.patwari.repository.AccountRepository
+import javax.transaction.Transactional
 
 @Service
 class AccountService(private val accountRepository: AccountRepository) {
@@ -18,5 +22,40 @@ class AccountService(private val accountRepository: AccountRepository) {
     fun createAccount(requestDto: AccountCreationRequest): AccountDetailsResponse {
         val account = accountRepository.save(requestDto.toAccountEntity())
         return AccountDetailsResponse(account.id, account.accountTitle, account.accountNumber, account.balance, account.accountType, account.status)
+    }
+
+    @Transactional
+    fun fundTransfer(requestDto: FundsTransferRequest) {
+
+        if(!requestDto.srcAccount.isNullOrEmpty()) {
+            val srcAcc = accountRepository.findByAccountNumber(requestDto.srcAccount!!)
+            debitAccount(srcAcc, requestDto.amount)
+        }
+
+        val destAcc = accountRepository.findByAccountNumber(requestDto.destAccount)
+        creditAccount(destAcc, requestDto.amount)
+    }
+
+    fun debitAccount(account: Account, amount: Double){
+        when(account.accountType){
+            AccountType.ASSETS, AccountType.EXPENSES -> {
+                accountRepository.addAmount(account.id, amount)
+            }
+            AccountType.EQUITY, AccountType.LIABILITIES, AccountType.REVENUE -> {
+                accountRepository.subtractAmount(account.id, amount)
+            }
+        }
+    }
+
+    fun creditAccount(account: Account, amount: Double){
+        var updatedAccount: Account
+        when(account.accountType){
+            AccountType.ASSETS, AccountType.EXPENSES -> {
+                updatedAccount = accountRepository.subtractAmount(account.id, amount)
+            }
+            AccountType.EQUITY, AccountType.LIABILITIES, AccountType.REVENUE -> {
+                updatedAccount = accountRepository.addAmount(account.id, amount)
+            }
+        }
     }
 }
--- src/test/kotlin/pk/com/patwari/service/AccountServiceTest.kt
+++ src/test/kotlin/pk/com/patwari/service/AccountServiceTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Captor
 import org.mockito.Mock
 import org.mockito.Mockito
@@ -16,6 +17,7 @@ import org.mockito.kotlin.whenever
 import pk.com.patwari.constant.AccountStatus
 import pk.com.patwari.constant.AccountType
 import pk.com.patwari.dto.request.AccountCreationRequest
+import pk.com.patwari.dto.request.FundsTransferRequest
 import pk.com.patwari.model.Account
 import pk.com.patwari.repository.AccountRepository
 
@@ -58,7 +60,7 @@ class AccountServiceTest{
         private val requestDto = AccountCreationRequest(accountNumber = "some-account-number", accountTitle = "some-account-title", accountType = AccountType.ASSETS)
         private val account = Account(id = "some-account-id", accountNumber = "some-account-number", accountTitle = "some-account-title", accountType = AccountType.ASSETS, status = AccountStatus.ACTIVE)
         @Test
-        fun shouldReturnAccountList(){
+        fun shouldReturnAccountCreated(){
 
             whenever(accountRepository.save(any(Account::class.java))).thenReturn(account)
 
@@ -73,6 +75,111 @@ class AccountServiceTest{
             assertEquals(accountCaptor.value.accountType, account.accountType)
             assertEquals(AccountStatus.ACTIVE, account.status)
 
+        }
+    }
+
+    @Nested
+    inner class FundTransfer{
+
+        private var requestDto = FundsTransferRequest(srcAccount = "some-src-account-number", destAccount = "some-dest-account-number", amount = 100.0, description = "")
+        private var srcAccount = getSourceAccount(AccountType.ASSETS)
+        private var destAccount = getDestAccount(AccountType.LIABILITIES)
+
+        @Test
+        fun `should only process for debit account when src account is empty`(){
+
+            requestDto = FundsTransferRequest(destAccount = "some-dest-account-number", amount = 100.0, description = "")
+
+            whenever(accountRepository.findByAccountNumber("some-dest-account-number")).thenReturn(destAccount)
+            whenever(accountRepository.addAmount("some-dest-account-id", 100.0)).thenReturn(destAccount)
+
+            service.fundTransfer(requestDto)
+
+            Mockito.verify(accountRepository, Mockito.times(1)).findByAccountNumber("some-dest-account-number")
+            Mockito.verify(accountRepository, Mockito.times(1)).addAmount("some-dest-account-id", 100.0)
+            Mockito.verify(accountRepository, Mockito.times(0)).subtractAmount("",0.0)
+        }
+
+        @Test
+        fun `should increase amount in both accounts when src is asset dest is liability`(){
+
+            whenever(accountRepository.findByAccountNumber("some-src-account-number")).thenReturn(srcAccount)
+            whenever(accountRepository.findByAccountNumber("some-dest-account-number")).thenReturn(destAccount)
+            whenever(accountRepository.addAmount("some-src-account-id", 100.0)).thenReturn(destAccount)
+            whenever(accountRepository.addAmount("some-dest-account-id", 100.0)).thenReturn(destAccount)
+
+            service.fundTransfer(requestDto)
+
+            Mockito.verify(accountRepository, Mockito.times(1)).findByAccountNumber("some-src-account-number")
+            Mockito.verify(accountRepository, Mockito.times(1)).findByAccountNumber("some-dest-account-number")
+            Mockito.verify(accountRepository, Mockito.times(1)).addAmount("some-src-account-id", 100.0)
+            Mockito.verify(accountRepository, Mockito.times(1)).addAmount("some-dest-account-id", 100.0)
+            Mockito.verify(accountRepository, Mockito.times(0)).subtractAmount("",0.0)
+        }
+
+        @Test
+        fun `should decrease amount in both accounts when src is liability dest is asset`(){
+            srcAccount = getSourceAccount(AccountType.LIABILITIES)
+            destAccount = getDestAccount(AccountType.ASSETS)
+
+            whenever(accountRepository.findByAccountNumber("some-src-account-number")).thenReturn(srcAccount)
+            whenever(accountRepository.findByAccountNumber("some-dest-account-number")).thenReturn(destAccount)
+            whenever(accountRepository.subtractAmount("some-src-account-id", 100.0)).thenReturn(destAccount)
+            whenever(accountRepository.subtractAmount("some-dest-account-id", 100.0)).thenReturn(destAccount)
+
+            service.fundTransfer(requestDto)
+
+            Mockito.verify(accountRepository, Mockito.times(1)).findByAccountNumber("some-src-account-number")
+            Mockito.verify(accountRepository, Mockito.times(1)).findByAccountNumber("some-dest-account-number")
+            Mockito.verify(accountRepository, Mockito.times(1)).subtractAmount("some-src-account-id", 100.0)
+            Mockito.verify(accountRepository, Mockito.times(1)).subtractAmount("some-dest-account-id", 100.0)
+            Mockito.verify(accountRepository, Mockito.times(0)).addAmount("",0.0)
+        }
+
+        @Test
+        fun `decrease amount liability as src acc and increase amount in revenue as dest acc`(){
+            srcAccount = getSourceAccount(AccountType.LIABILITIES)
+            destAccount = getDestAccount(AccountType.REVENUE)
+
+            whenever(accountRepository.findByAccountNumber("some-src-account-number")).thenReturn(srcAccount)
+            whenever(accountRepository.findByAccountNumber("some-dest-account-number")).thenReturn(destAccount)
+            whenever(accountRepository.subtractAmount("some-src-account-id", 100.0)).thenReturn(destAccount)
+            whenever(accountRepository.addAmount("some-dest-account-id", 100.0)).thenReturn(destAccount)
+
+            service.fundTransfer(requestDto)
+
+            Mockito.verify(accountRepository, Mockito.times(1)).findByAccountNumber("some-src-account-number")
+            Mockito.verify(accountRepository, Mockito.times(1)).findByAccountNumber("some-dest-account-number")
+            Mockito.verify(accountRepository, Mockito.times(1)).subtractAmount("some-src-account-id", 100.0)
+            Mockito.verify(accountRepository, Mockito.times(1)).addAmount("some-dest-account-id", 100.0)
+        }
+
+        @Test
+        fun `increase amount of asset as src acc and decrease amount of expense as dest acc`(){
+            srcAccount = getSourceAccount(AccountType.ASSETS)
+            destAccount = getDestAccount(AccountType.EXPENSES)
+
+            whenever(accountRepository.findByAccountNumber("some-src-account-number")).thenReturn(srcAccount)
+            whenever(accountRepository.findByAccountNumber("some-dest-account-number")).thenReturn(destAccount)
+            whenever(accountRepository.addAmount("some-src-account-id", 100.0)).thenReturn(destAccount)
+            whenever(accountRepository.subtractAmount("some-dest-account-id", 100.0)).thenReturn(destAccount)
+
+            service.fundTransfer(requestDto)
+
+            Mockito.verify(accountRepository, Mockito.times(1)).findByAccountNumber("some-src-account-number")
+            Mockito.verify(accountRepository, Mockito.times(1)).findByAccountNumber("some-dest-account-number")
+            Mockito.verify(accountRepository, Mockito.times(1)).addAmount("some-src-account-id", 100.0)
+            Mockito.verify(accountRepository, Mockito.times(1)).subtractAmount("some-dest-account-id", 100.0)
+        }
+    }
+
+    companion object{
+        fun getSourceAccount(accountType: AccountType): Account{
+            return Account(id = "some-src-account-id", accountNumber = "some-src-account-number", accountTitle = "some-account-title", accountType = accountType, status = AccountStatus.ACTIVE)
+        }
+
+        fun getDestAccount(accountType: AccountType): Account{
+            return Account(id = "some-dest-account-id", accountNumber = "some-dest-account-number", accountTitle = "some-account-title", accountType = accountType, status = AccountStatus.ACTIVE)
         }
     }
 }
